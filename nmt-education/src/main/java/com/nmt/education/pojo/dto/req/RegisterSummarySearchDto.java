7253ca499eefc6762e21366e05863f6d0c676068
peterchen <peterchenzhy@aliyun.com>
2020-05-04 23:03:26 +0800

    fix 查询日期失效的情况

--- nmt-education/src/main/java/com/nmt/education/pojo/dto/req/RegisterSummarySearchDto.java
+++ nmt-education/src/main/java/com/nmt/education/pojo/dto/req/RegisterSummarySearchDto.java
@@ -1,5 +1,6 @@
 package com.nmt.education.pojo.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -38,22 +39,26 @@ public class RegisterSummarySearchDto {
      * 上课日期 -- 开始
      */
     @ApiModelProperty(value = "上课日期 -- 开始")
+    @JsonFormat( pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date startDate;
     /**
      * 上课日期-- 结束
      */
     @ApiModelProperty(value = "上课日期-- 结束")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date endDate;
 
     /**
      * 报名日期 -- 开始
      */
     @ApiModelProperty(value = "报名日期 -- 开始")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date registerStartDate;
     /**
      * 报名日期-- 结束
      */
     @ApiModelProperty(value = "报名日期-- 结束")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date registerEndDate;
 
     /**
